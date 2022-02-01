0a4001281c2cfdc744b9c800a7747f321d5bf649
peah <padecallonne@gmail.com>
2022-02-01 16:24:16 +0100

    gerer la gestion de fermeture de modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,11 +6,11 @@ function Modal({setModal})  {
 
         return (
         <div className='z-50 absolute top-0 bottom-0 left-0 right-0'>
-            <div className="fixed py-12 h-full bg-gradient-to-br from-red-600  to-orange-400 opacity-60 transition duration-150 ease-in-out z-10 top-0 right-0 bottom-0 left-0" id="modal"></div>
+            <div onClick={handleClose} className="fixed py-12 h-full bg-gradient-to-br from-red-600  to-orange-400 opacity-60 transition duration-150 ease-in-out z-10 top-0 right-0 bottom-0 left-0" id="modal"></div>
                 <div role="alert" className="container w-full flex m-auto absolute top-0 bottom-0 left-0 right-0">
                     <div className="relative z-50 py-8 px-5 md:px-10 w-full m-auto bg-white shadow-md rounded border border-gray-400">
                         <div className="w-full flex justify-start text-gray-600 mb-3">
-                            <svg onClick={handleClose} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-wallet" width="52" height="52" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-wallet" width="52" height="52" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
                                 <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
@@ -60,9 +60,9 @@ function Modal({setModal})  {
                         </div>
                         <div className="flex items-center justify-start w-full">
                             <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
-                            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onclick="modalHandler()">Cancel</button>
+                            <button onClick={handleClose} className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onclick="modalHandler()">Cancel</button>
                         </div>
-                        <button className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
+                        <button onClick={handleClose} className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onclick="modalHandler()" aria-label="close modal" role="button">
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <line x1="18" y1="6" x2="6" y2="18" />
